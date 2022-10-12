7c12a07100311f1538a8613fa6c8eb008176f9c8
Badziev Alan <badjity@yandex.ru>
2022-10-12 19:14:39 +0300

    Nullable types

--- src/main/kotlin/jav/Main.java
+++ src/main/kotlin/jav/Main.java
@@ -1,7 +1,9 @@
 package jav;
 
 public class Main {
+    private static String name = null;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(name.length());
     }
 }
--- src/main/kotlin/kot/Main.kt
+++ src/main/kotlin/kot/Main.kt
@@ -1,6 +1,13 @@
 package kot
 
+private var String1:String? = "sd6s"
+private var String2:String? = "33"
+private var String3:String? = null
 
 fun main() {
-    println("Hello")
+
+    val result = (String1?.length?:0) + (String2?.length?:0) + (String3?.length?:0)
+
+    println(result)
+
 }
